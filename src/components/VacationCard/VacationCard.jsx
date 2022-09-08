48deca84793070068ac1e8b8929e9a222ca0ea91
username <youremail@domain.com>
2022-09-08 15:05:01 -0700

    working pages

--- src/components/VacationCard/VacationCard.jsx
+++ src/components/VacationCard/VacationCard.jsx
@@ -2,7 +2,11 @@ export default function VacationIndexPage({ vacations }) {
   console.log(vacations);
   return (
     <>
+      <img src={vacations.image}></img>
       <h1>{vacations.name}</h1>
+      <h2>{vacations.location}</h2>
+      <h2>{vacations.activities}</h2>
+      <p>{vacations.pricing}</p>
     </>
   );
 }
--- src/pages/VacationIndexPage/VacationIndexPage.jsx
+++ src/pages/VacationIndexPage/VacationIndexPage.jsx
@@ -3,7 +3,7 @@ export default function VacationDetailsPage({ vacations }) {
   console.log(vacations);
   return (
     <div>
-      <h1>My Vacations</h1>;
+      <h1>My Vacations</h1>
       <ul>
         {vacations.map((vacations) => (
           <VacationCard vacations={vacations} />
